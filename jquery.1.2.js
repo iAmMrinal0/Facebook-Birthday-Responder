3614ae963c2aeae70d00b511fac017e6ef482086
Mrinal Purohit <mrinal1892005@gmail.com>
2012-06-25 04:14:41 +0530

    bugger

--- jquery.1.2.js
+++ jquery.1.2.js
@@ -1,3 +1,9 @@
+var auto_refresh = setInterval(
+function()
+    {
+    $('#reff').fadeOut().load('ref.php').fadeIn();
+
+    }, 10000);
 $(document).ready(function(){
 	$(".box").hide();
 	$(".click").click(function(){
