a72e002ce4e23eb07a640b1cd7b8ac785725a889
Mrinal Purohit <mrinal1892005@gmail.com>
2012-06-25 03:56:37 +0530

    released

--- jquery.1.2.js
+++ jquery.1.2.js
@@ -1,10 +1,3 @@
-$(document).ready(function() {
- 	 $("#reff").load("ref.php");
-   var refreshId = setInterval(function() {
-      $("#reff").load('ref.php');
-   }, 6000);
-   $.ajaxSetup({ cache: false });
-});
 $(document).ready(function(){
 	$(".box").hide();
 	$(".click").click(function(){
