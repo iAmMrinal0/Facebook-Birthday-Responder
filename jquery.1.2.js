342146da0b312182e09f3f18bc130352bc7b36e6
Mrinal Purohit <mrinal1892005@gmail.com>
2012-06-25 11:22:11 +0530

    changed file

--- jquery.1.2.js
+++ jquery.1.2.js
@@ -1,8 +1,3 @@
-var auto_refresh = setInterval(
-function()
-{
-$('#reff').fadeOut('slow').load('ref.php').fadeIn("slow");
-}, 10000);
 $(document).ready(function(){
 	$(".box").hide();
 	$(".click").click(function(){
